01651bf311a8b369781efa67c5c8f4feda3bdc1b
djmattyg007 <mwgam36@gmail.com>
2014-03-30 16:04:25 +1100

    Implemented caching for translations.

--- src/app/framework/Helper/Core/Translate.php
+++ src/app/framework/Helper/Core/Translate.php
@@ -7,6 +7,8 @@ use \MattyG\Framework\Core\Helper\HelperInterface as Helper;
 
 class Translate implements Helper
 {
+    const LOCALE_CACHE_ENTRY_NAME = "core_locale";
+
     const DIR_LOCALE = "locale";
 
     /**
@@ -49,12 +51,53 @@ class Translate implements Helper
     }
 
     /**
+     * Save all available translations into the cache.
+     *
      * @param string $locale
+     * @return void
+     */
+    protected function saveLocaleCache()
+    {
+        if (!($cache = $this->config->getCacheObject())) {
+            return;
+        }
+        $cache->saveData(self::LOCALE_CACHE_ENTRY_NAME, json_encode($this->translations), (time() + 3600));
+    }
+
+    /**
+     * Load translations from the cache so we don't need to read them all from
+     * disk individually.
+     *
+     * @return array|null
+     */
+    protected function loadLocaleCache()
+    {
+        if (!($cache = $this->config->getCacheObject())) {
+            return null;
+        }
+        return json_decode($cache->loadData(self::LOCALE_CACHE_ENTRY_NAME, null), true);
+    }
+
+    /**
+     * Read in all translations for a given locale. Defaults to the locale
+     * specified in the configuration tree.
+     * Optionally checks to see if translations are available in the cache
+     * first, and if so, uses that rather than re-read all translations from
+     * the disk again.
+     *
+     * @param string $locale
+     * @param bool $useCache
+     * @return void
      * @throws \InvalidArgumentException
      * @throws \RuntimeException
      */
-    public function loadTranslations($locale = null)
+    public function loadTranslations($locale = null, $useCache = true)
     {
+        if ($useCache === true && ($translations = $this->loadLocaleCache())) {
+            $this->translations = $translations;
+            return;
+        }
+
         if (!is_string($locale)) {
             if ($locale === null) {
                 $locale = $this->locale;
@@ -93,6 +136,7 @@ class Translate implements Helper
             }
             $this->translations = array_merge($this->translations, $translations);
         }
+        $this->saveLocaleCache();
     }
 
     /**

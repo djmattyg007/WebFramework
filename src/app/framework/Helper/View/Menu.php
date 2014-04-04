732fe194de032958d8219ca01450c6ecd22539fb
djmattyg007 <mwgam36@gmail.com>
2014-04-05 08:58:56 +1100

    Menus will now be able to tell if a requested route is the "active" route

--- src/app/framework/Helper/View/Menu.php
+++ src/app/framework/Helper/View/Menu.php
@@ -2,10 +2,10 @@
 
 namespace MattyG\Framework\Helper\View;
 
-use \MattyG\Framework\Core\Helper\HelperInterface as Helper;
+use \MattyG\Framework\Core\Helper\HelperInterfaceExtra as HelperExtra;
 use \MattyG\Framework\Core\Config as Config;
 
-class Menu implements Helper
+class Menu implements HelperExtra
 {
     /**
      * @var Config
@@ -13,9 +13,9 @@ class Menu implements Helper
     protected $config;
 
     /**
-     * @var MattyG\Framework\Helper\Core\Url
+     * @var \Aura\Router\Router
      */
-    protected $urlHelper;
+    protected $router;
 
     /**
      * @var array
@@ -30,6 +30,21 @@ class Menu implements Helper
     {
         $this->config = $config;
         $this->loadMenuConfig($helperName);
+    }
+
+    /**
+     * @param array $helpers
+     */
+    public function giveHelpers(array $helpers)
+    {
+        foreach ($helpers as $helper => $instance) {
+            switch ($helper)
+            {
+                case "router":
+                    $this->router = $instance;
+                    break;
+            }
+        }
     }
 
     /**
@@ -50,6 +65,45 @@ class Menu implements Helper
     public function getMenuItems()
     {
         return $this->menuConfig;
+    }
+
+    /**
+     * @param string name
+     * @param array $params
+     * @return bool
+     */
+    public function isActiveRoute($routeName, array $params = array())
+    {
+        $matchedRoute = $this->router->getMatchedRoute();
+        if (!$matchedRoute) {
+            // This means that either matching hasn't taken place, or that no
+            // match was found.
+            return false;
+        }
+        if ($matchedRoute->name !== $routeName) {
+            // Wrong route name, pure and simple.
+            return false;
+        }
+
+        // In order to ensure this is an exact match, we need to check any
+        // matched parameters on the route.
+        $namedParams = stripNumericKeys($matchedRoute->matches);
+        foreach ($namedParams as $key => $value) {
+            if (!isset($params[$key])) {
+                return false;
+            }
+            if ($params[$key] != $value) {
+                return false;
+            }
+            unset($params[$key]);
+        }
+        // If there are unchecked parameters in the user-supplied array, it
+        // probably means the route they were checking against had optional
+        // parameters that were not supplied in the request URI.
+        if (count($params)) {
+            return false;
+        }
+        return true;
     }
 }
 

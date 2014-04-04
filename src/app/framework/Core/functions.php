71eeb687e9a85a460bb973325cc0a1f48d4218a9
djmattyg007 <mwgam36@gmail.com>
2014-04-05 08:56:27 +1100

    Added two new core functions.
    The functions are designed to strip numeric keys and associative keys
    from arrays.

--- src/app/framework/Core/functions.php
+++ src/app/framework/Core/functions.php
@@ -19,6 +19,8 @@ function isFloat($number)
  * type integer. If there is at least one key that is of type string, the array
  * is considered to be an associative array.
  *
+ * This function has a Big-O time complexity of O(2N).
+ *
  * @param array $array
  * @return bool
  */
@@ -26,3 +28,40 @@ function isAssoc(array $array)
 {
     return (bool) count(array_filter(array_keys($array), "is_string"));
 }
+
+/**
+ * Iterate over all keys in the array and remove any that are non-numeric.
+ *
+ * This function has a Big-O time complexity of O(N).
+ *
+ * @param array $array
+ * @return array
+ */
+function stripAssocKeys(array $array)
+{
+    foreach ($array as $key => $value) {
+        if (!is_int($key)) {
+            unset($array[$key]);
+        }
+    }
+    return $array;
+}
+
+/**
+ * Iterate over all keys in the array and remove any that are numeric.
+ *
+ * This function has a Big-O time complexity of O(N).
+ *
+ * @param array $array
+ * @return array
+ */
+function stripNumericKeys(array $array)
+{
+    foreach ($array as $key => $value) {
+        if (is_int($key)) {
+            unset($array[$key]);
+        }
+    }
+    return $array;
+}
+

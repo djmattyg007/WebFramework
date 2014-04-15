4ea115be047ce38fabbbcdbfa96455c227b52911
djmattyg007 <git@matthewgamble.net>
2014-04-15 22:33:08 +1000

    Add DB setter for DatabaseCollection.
    Even though DatabaseCollection uses Database, Database won't show up in
    the return array from class_uses(). Why, PHP? Why?

--- src/app/framework/di.php
+++ src/app/framework/di.php
@@ -3,4 +3,5 @@
 /** @var $di \Aura\Di\Container */
 
 $di->setter["MattyG\Framework\Core\Entity\Database"]["setDB"] = $di->get("db");
+$di->setter["MattyG\Framework\Core\Entity\DatabaseCollection"]["setDB"] = $di->get("db");
 

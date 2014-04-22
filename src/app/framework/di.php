ef7ed8eee76abd5a7869ab960f3e1ba542669298
djmattyg007 <git@matthewgamble.net>
2014-04-23 00:44:52 +1000

    Update DI container dependency.
    It now has a patch I submitted that helps reduce code duplication
    throughout the application.

--- src/app/framework/di.php
+++ src/app/framework/di.php
@@ -3,7 +3,6 @@
 /** @var $di \Aura\Di\Container */
 
 $di->setter["MattyG\\Framework\\Core\\Entity\\Database"]["setDB"] = $di->get("db");
-$di->setter["MattyG\\Framework\\Core\\Entity\\DatabaseCollection"]["setDB"] = $di->get("db");
 
 // Helpers
 // I have to use closures here because the DI container will throw an exception
